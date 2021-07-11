eb04eaeb54740204899212f093861e5bc0e308b4
meimecaj <meimecaj@gmail.com>
2021-07-11 15:26:34 +0200

    feat(package): add apollo client package for query usage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const TEST = gql`
+  query GetSpecificSong {
+    lookup {
+      releaseGroup(mbid: "99599db8-0e36-4a93-b0e8-350e9d7502a9") {
+        title
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(TEST);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+
   return (
     <div>
-      <p>Works!</p>
+      <p>{data.lookup.releaseGroup.title}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
